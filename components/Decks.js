5973bc3be1ec34a6efbaf95c2339331d70430805
nagamohanm <nagamohan.magadi@gmail.com>
2017-11-08 00:59:10 +0530

    Refactored render function

--- components/Decks.js
+++ components/Decks.js
@@ -1,8 +1,14 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { StackNavigator } from 'react-navigation'
-import { Asset, AppLoading, Font } from 'expo';
-import { View, TouchableOpacity, Text, TextInput, KeyboardAvoidingView } from 'react-native'
+import { Asset, AppLoading, Font } from 'expo'
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  TextInput,
+  KeyboardAvoidingView
+} from 'react-native'
 import styled from 'styled-components/native'
 
 import {
@@ -20,55 +26,73 @@ import { receiveDecks } from '../actions'
 import { white } from '../utils/colors'
 
 class Decks extends Component {
-
   state = {
-    isReady: false,
+    isReady: false
   }
 
   async _getDataAndCacheResourcesAsync(dispatch) {
-    await getDecks().then((decks) => dispatch(receiveDecks(decks)))
+    await getDecks().then(decks => dispatch(receiveDecks(decks)))
     await Font.loadAsync({
-      'roboto-light': require('../assets/fonts/Roboto-Light.ttf'),
+      'roboto-light': require('../assets/fonts/Roboto-Light.ttf')
     })
   }
 
   componentWillUpdate() {
-    getDecks().then((decks) => this.props.dispatch(receiveDecks(decks)))
+    getDecks().then(decks => this.props.dispatch(receiveDecks(decks)))
+  }
+
+  loadingApp = () => {
+    return (
+      <AppLoading
+        startAsync={() =>
+          this._getDataAndCacheResourcesAsync(this.props.dispatch)}
+        onFinish={() => this.setState({ isReady: true })}
+        onError={console.warn}
+      />
+    )
+  }
+
+  decksListView = () => {
+    const { decks, navigation } = this.props
+    return (
+      <GreenView>
+        <GridTop>
+          {Object.keys(decks).map(deckname => (
+            <DeckView
+              key={deckname}
+              onPress={() =>
+                navigation.navigate('Deck', { title: decks[deckname].title })}
+            >
+              <DeckTitleText>{decks[deckname].title}</DeckTitleText>
+              <DeckSubTitleText>
+                {decks[deckname].questions.length} Card(s)
+              </DeckSubTitleText>
+            </DeckView>
+          ))}
+        </GridTop>
+        <GridFooter>
+          <ButtonGreen onPress={() => navigation.navigate('addDeck')}>
+            <ButtonText>Add Deck</ButtonText>
+          </ButtonGreen>
+        </GridFooter>
+      </GreenView>
+    )
   }
 
   render() {
-    const { decks, navigation } = this.props
+    let DecksView = null
+
     if (!this.state.isReady) {
-      return(
-        <AppLoading
-          startAsync={() => this._getDataAndCacheResourcesAsync(this.props.dispatch)}
-          onFinish={() => this.setState({ isReady: true })}
-          onError={console.warn}
-        />
-      )
+      DecksView = this.loadingApp
     } else {
-      return(
-        <GreenView>
-          <GridTop>
-            {Object.keys(decks).map((deckname) => (
-              <DeckView key={deckname} onPress={() => navigation.navigate('Deck', { title: decks[deckname].title })}>
-                <DeckTitleText>{decks[deckname].title}</DeckTitleText>
-                <DeckSubTitleText>{decks[deckname].questions.length} Card(s)</DeckSubTitleText>
-              </DeckView>
-            ))}
-          </GridTop>
-          <GridFooter>
-            <ButtonGreen onPress={() => navigation.navigate('addDeck')}>
-              <ButtonText>Add Deck</ButtonText>
-            </ButtonGreen>
-          </GridFooter>
-        </GreenView>
-      )
+      DecksView = this.decksListView
     }
+
+    return <DecksView />
   }
 }
 
-function mapStateToProps (decks) {
+function mapStateToProps(decks) {
   return {
     decks
   }
