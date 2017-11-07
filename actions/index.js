d3884d2aaefa20ef80990222064642dcc620bda3
nagamohanm <nagamohan.magadi@gmail.com>
2017-11-08 01:05:02 +0530

    Moved action constants to separate file

--- actions/index.js
+++ actions/index.js
@@ -1,35 +1,37 @@
-export const RECEIVE_DECKS = 'RECEIVE_DECKS'
-export const CREATE_NEW_DECK = 'CREATE_NEW_DECK'
-export const CREATE_NEW_CARD = 'CREATE_NEW_CARD'
-export const UPDATE_SUCCESS = 'UPDATE_SUCCESS'
+import {
+  RECEIVE_DECKS,
+  CREATE_NEW_DECK,
+  CREATE_NEW_CARD,
+  UPDATE_SUCCESS
+} from './types'
 
-export function receiveDecks (decks) {
+export function receiveDecks(decks) {
   return {
     type: RECEIVE_DECKS,
-    decks,
+    decks
   }
 }
 
-export function createNewDeck (title) {
+export function createNewDeck(title) {
   return {
     type: CREATE_NEW_DECK,
-    title,
+    title
   }
 }
 
-export function createNewCard (deckName, question, answer) {
+export function createNewCard(deckName, question, answer) {
   return {
     type: CREATE_NEW_CARD,
     deckName,
     question,
-    answer,
+    answer
   }
 }
 
-export function updateSuccess (deckName, correct){
-  return{
+export function updateSuccess(deckName, correct) {
+  return {
     type: UPDATE_SUCCESS,
     deckName,
-    correct,
+    correct
   }
 }
--- reducers/index.js
+++ reducers/index.js
@@ -2,25 +2,28 @@ import {
   RECEIVE_DECKS,
   CREATE_NEW_DECK,
   CREATE_NEW_CARD,
-  UPDATE_SUCCESS,
-} from "../actions"
-import { getDecks } from "../utils/api"
+  UPDATE_SUCCESS
+} from '../actions/types'
+import { getDecks } from '../utils/api'
 
-function decks (state = {}, action){
-  switch(action.type){
+function decks(state = {}, action) {
+  switch (action.type) {
     case RECEIVE_DECKS:
       return action.decks
     case CREATE_NEW_DECK:
-      return ( {
+      return {
         ...state,
         [action.title]: {
           title: action.title,
           questions: [],
-          correct: 0,
+          correct: 0
         }
-      })
+      }
     case CREATE_NEW_CARD:
-      state[action.deckName].questions.push({question: action.question, answer: action.answer})
+      state[action.deckName].questions.push({
+        question: action.question,
+        answer: action.answer
+      })
       return state
     case UPDATE_SUCCESS:
       state[action.deckName].correct = action.correct
